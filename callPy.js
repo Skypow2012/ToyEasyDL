41d70286bef237d23b57ce6dd54990f45f66d83a
gaifang <7605111+gaifang@user.noreply.gitee.com>
2020-12-20 04:05:44 +0800

    py => python3

--- callPy.js
+++ callPy.js
@@ -3,7 +3,7 @@ const exec = require('child_process').exec;
 function getVec(imgPath) {
   return new Promise((resolve, reject) => {
     console.log(`推导：./${imgPath}`)
-    const cmdStr = `py ./py/easydl.py --test ./${imgPath}`;
+    const cmdStr = `python3 ./py/easydl.py --test ./${imgPath}`;
     exec(cmdStr, function (err, stdout, stderr) {
       if (err) {
         reject(stderr);
